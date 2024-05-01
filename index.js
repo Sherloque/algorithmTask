c7f2528346cb8c0890c1fc0fb48b92a63decf0b8
Sherloque <33927258+Sherloque@users.noreply.github.com>
2024-05-01 15:00:53 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+function createArray(n) {
+    let array = [];
+    for (let i = 1; i <= n; i++) {
+        array.push(i);
+    }
+    return array;
+}
+
+function removeRandomElements(array) {
+    let index1 = Math.floor(Math.random() * array.length);
+    let index2 = Math.floor(Math.random() * array.length);
+    while (index2 === index1) {
+        index2 = Math.floor(Math.random() * array.length);
+    }
+    array.splice(index1, 1);
+    array.splice(index2 < index1 ? index2 : index2 - 1, 1);
+}
+
+function findMissingNumbers(array) {
+    let n = array.length + 2;
+    let expectedSum = (n * (n + 1)) / 2;
+    let actualSum = array.reduce((acc, curr) => acc + curr, 0);
+    let missingSum = expectedSum - actualSum;
+
+    let presentNumbers = {};
+    for (let num of array) {
+        presentNumbers[num] = true;
+    }
+
+    let firstMissingNumber;
+    let secondMissingNumber;
+    for (let i = 1; i <= missingSum; i++) {
+        if (!presentNumbers[i] && !firstMissingNumber) {
+            firstMissingNumber = i;
+        } else if (!presentNumbers[i]) {
+            secondMissingNumber = i;
+            break;
+        }
+    }
+
+    return [firstMissingNumber, secondMissingNumber];
+}
+
+const number = parseInt(prompt("Введите число (больше 1):"));
+
+if (!isNaN(number) && number > 1) {
+    let sequentialArray = createArray(number);
+    removeRandomElements(sequentialArray);
+    let missingNumbers = findMissingNumbers(sequentialArray);
+
+    console.log(`Исходный массив от 1 до ${number}:`, sequentialArray);
+    console.log("Отсутствующие числа:", missingNumbers);
+} else {
+    console.log("Пожалуйста, введите корректное число больше 1.");
+}
+
+
+/*
+Вычислительная сложность решения - O(n), где n - количество элементов в массиве, сложность будет линейно расти вместе с размером исходного массива
+*/
